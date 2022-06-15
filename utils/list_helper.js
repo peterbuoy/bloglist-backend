146c3255fd8ab57628489dd25039d7dc81e252cf
peter <peterbuoy@gmail.com>
2022-06-15 03:13:57 -0700

    add tests for favorite blog function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,17 @@
 const blog = require("../models/blog");
+const Blog = require("../models/blog");
+
+/**
+ * @param {[Blog]} blogs
+ **/
 
 const dummy = (blogs) => {
   return 1;
 };
 
+/**
+ * @param {[Blog]} blogs
+ **/
 const totalLikes = (blogs) => {
   const reducer = (likeSum, blog) => {
     return likeSum + blog.likes;
@@ -11,4 +19,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
-module.exports = { dummy, totalLikes };
+/**
+ * @param {[Blog]} blogs
+ **/
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+  let favoriteBlogIndex = 0;
+  let maxLikes = 0;
+  blogs.forEach((blog, index) => {
+    if (blog.likes > maxLikes) {
+      favoriteBlogIndex = index;
+      maxLikes = blog.likes;
+    }
+  });
+  return blogs[favoriteBlogIndex];
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
