4ff7376f220b1b8cd96edee66a89e194a34fc798
peter <peterbuoy@gmail.com>
2022-06-15 02:37:58 -0700

    create unit tests for totalLikes function

--- tests/list.test.js
+++ tests/list.test.js
@@ -7,6 +7,10 @@ test("dummy returns one", () => {
 });
 
 describe("total likes", () => {
+  const noBlogs = [];
+  test("of empty list is zero", () => {
+    expect(listHelper.totalLikes(noBlogs)).toBe(0);
+  });
   const blogs = [
     {
       _id: "5a422a851b54a676234d17f7",
@@ -57,7 +61,22 @@ describe("total likes", () => {
       __v: 0,
     },
   ];
-  test("of empty list is zero", () => {
+  test("of a bigger list is calculated right", () => {
     expect(listHelper.totalLikes(blogs)).toBe(36);
+  });
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
   });
 });
